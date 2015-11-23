457d22b179ec5c625da7058a67aedbc0e9fa27e6
flavien <flavien-lecuyer@hotmail.fr>
2015-11-23 18:23:22 +0100

    Added exceptions

---
+++ PooP/PooP.Core/Exceptions/BadPositionException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooP.Core.Exceptions
+{
+    public class BadPositionException : Exception
+    {
+        public BadPositionException() : base("Position out of bounds !")
+        {
+            
+        }
+    }
+}
