b11a8941187d1bb1d136a3416fee7880693e93dd
Loic Pelleau <loic.pelleau@insa-rennes.fr>
2015-12-02 11:16:15 +0100

    clean

---
+++ PooP/PooP.Core/Resource/WrapperMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooP.Core.Ressource
+{
+    public enum TileType
+    {
+        Plain = 0,
+        Mountain = 1,
+        Forest = 2,
+        Water = 3
+    }
+
+    public class WMap
+    {
+        public WMap(TileType[] tiles)
+        {
+            Tiles = tiles;
+        }
+        public TileType[] Tiles { get; private set; }
+    }
+}
