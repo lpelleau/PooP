49890e872381b30e701a3282dabe67f3b3f27d7d
flavien <flavien-lecuyer@hotmail.fr>
2015-12-12 22:09:43 +0100

    Adding quit command and moving p2 image

--- PooP/PooP.GUI/MainViewModel.cs
+++ PooP/PooP.GUI/MainViewModel.cs
@@ -69,6 +69,15 @@ namespace Interface
             }
         }
 
+        // Command to quit
+        public ICommand Quit
+        {
+            get
+            {
+                return new QuitCommand(this);
+            }
+        }
+
         // Values for game creation
 
         public string Player1Name
@@ -350,6 +359,51 @@ namespace Interface
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+
+    /// <summary>
+    /// Command for quitting the game
+    /// </summary>
+    public class QuitCommand : ICommand
+    {
+        private MainViewModel mvm;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="m">Associated ViewModel</param>
+        public QuitCommand(MainViewModel m)
+        {
+            mvm = m;
+        }
+
+        /// <summary>
+        /// Quits
+        /// </summary>
+        /// <param name="o">Current window</param>
+        public void Execute(Object o)
+        {
+            ((MainWindow)o).Close();
+        }
+
+        /// <summary>
+        /// Tests if the game can be exited
+        /// </summary>
+        /// <param name="o">Unused</param>
+        /// <returns>true</returns>
+        public bool CanExecute(Object o)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Never called since CanExecute is constant
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
         }
     }
 }
